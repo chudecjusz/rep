c3d3342d33671f41fdef3d17804e12791bd92358
SAWDR\Sylwek <sylwester.adamczak.76@gmail.com>
2017-03-20 13:36:59 +0100

    no message

---
+++ HOST.IVENDinvoice/HOST.IVENDinvoice.Invoices/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HOST.IVENDinvoice.Invoices.Model;
+
+namespace HOST.IVENDinvoice.Invoices.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
